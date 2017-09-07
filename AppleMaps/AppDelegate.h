24f5322b564c0b3df2e90ee7403b4a95801a1b74
kishore kumar nagalla <kishorekumarnagalla@Mac-MIni-PSS.local>
2017-09-07 11:25:41 +0530

    comment

--- AppleMaps/AppDelegate.h
+++ AppleMaps/AppDelegate.h
@@ -9,6 +9,9 @@
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
 
+
+// line added in appdelegate
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
